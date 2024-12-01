9d46f5feb0a5884a3203ad2029cc0243c7dc8066
Xwilarg <xwilarg@protonmail.com>
2024-12-01 12:13:41 +0000

    Add day 1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+List<int> l1 = new();
+List<int> l2 = new();
+
+foreach (var d in File.ReadAllLines("data/day01.txt"))
+{
+    var m = Regex.Match(d, "([0-9]+) +([0-9]+)");
+
+    l1.Add(int.Parse(m.Groups[1].Value));
+    l2.Add(int.Parse(m.Groups[2].Value));
+}
+
+l1 = [.. l1.Order()];
+l2 = [.. l2.Order()];
+
+var res = Enumerable.Range(0, l1.Count).Sum(i => Math.Abs(l1[i] - l2[i]));
+Console.WriteLine(res);
