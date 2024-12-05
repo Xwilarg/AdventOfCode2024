95c095b24af37b72a3dc9478d0779f03004be195
Xwilarg <xwilarg@protonmail.com>
2024-12-05 13:11:44 +0000

    Add d5p2

--- Day/Day05.cs
+++ Day/Day05.cs
@@ -8,15 +8,36 @@
             var instructions = lines[0].Split('\n');
             var books = lines[1].Split('\n');
 
-            int count = 0;
+            int countP1 = 0;
+            int countP2 = 0;
 
             foreach (var b in books)
             {
                 var pages = b.Split(',');
-                if (Enumerable.Range(0, pages.Length - 1).All(i => !instructions.Contains($"{pages[i + 1]}|{pages[i]}"))) count += int.Parse(pages[pages.Length / 2]);
+                if (Enumerable.Range(0, pages.Length - 1).All(i => !instructions.Contains($"{pages[i + 1]}|{pages[i]}"))) countP1 += int.Parse(pages[pages.Length / 2]);
+                else
+                {
+                    bool isValid;
+
+                    do
+                    {
+                        isValid = true;
+                        for (int i = 0; i < pages.Length - 1; i++)
+                        {
+                            if (instructions.Contains($"{pages[i + 1]}|{pages[i]}"))
+                            {
+                                (pages[i + 1], pages[i]) = (pages[i], pages[i + 1]);
+                                isValid = false;
+                                break;
+                            }
+                        }
+                    } while (!isValid);
+                    countP2 += int.Parse(pages[pages.Length / 2]);
+                }
             }
 
-            Console.WriteLine(count);
+            Console.WriteLine(countP1);
+            Console.WriteLine(countP2);
         }
     }
 }
